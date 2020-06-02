c40c57269ac91ecf28dd2dba4158bc14eb1dcbe9
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-06-02 21:45:19 +0200

    refactor: ♻️ remove unused code

--- src/util.ts
+++ src/util.ts
@@ -1,30 +1,30 @@
 import * as GLib from "./types/GLib-2.0"
 
-/**
- * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
- * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval
- */
-export const setInterval = (func: () => unknown, delay: number) => {
-  return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, () => {
-    func()
-    return GLib.SOURCE_CONTINUE
-  })
-}
+// /**
+//  * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+//  * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval
+//  */
+// export const setInterval = (func: () => unknown, delay: number) => {
+//   return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, () => {
+//     func()
+//     return GLib.SOURCE_CONTINUE
+//   })
+// }
 
-export const clearInterval = GLib.source_remove
+// export const clearInterval = GLib.source_remove
 
-/**
- * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout
- * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/clearTimeout
- */
-export const setTimeout = (func: () => unknown, delay: number) => {
-  return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, () => {
-    func()
-    return GLib.SOURCE_REMOVE
-  })
-}
+// /**
+//  * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout
+//  * https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/clearTimeout
+//  */
+// export const setTimeout = (func: () => unknown, delay: number) => {
+//   return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, () => {
+//     func()
+//     return GLib.SOURCE_REMOVE
+//   })
+// }
 
-export const clearTimeout = GLib.source_remove
+// export const clearTimeout = GLib.source_remove
 
 export function fileExists(file: string) {
   return (
