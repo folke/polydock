52b9d75d149e546e7a21c8d3d15ec1289f7576e0
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-06-03 15:42:59 +0200

    build: 📦️ added imports.system.refcount

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,9 +1,12 @@
+import * as GObject from "./types/GObject-2.0"
+
 declare global {
   const ARGV: string[]
   const imports: {
     system: {
       gc: () => void
       exit: (exitCode) => void
+      refcount: (obj: GObject.Object) => number
       programInvocationName: string
     }
   }
